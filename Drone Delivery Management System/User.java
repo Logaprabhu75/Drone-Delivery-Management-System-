7c4a73ae115893ae08fa9259cfc0637a722de1b0
Logaprabhu75 <logaprabhu549@gmail.com>
2025-09-21 01:26:32 +0530

    Update User.java

--- Drone Delivery Management System/User.java
+++ Drone Delivery Management System/User.java
@@ -1,19 +1,17 @@
 public class User {
     protected int userId;
     protected String name;
-
     public User(int userId, String name) {
         this.userId = userId;
         this.name = name;
     }
-
     public void login() {
         System.out.println(name + "  you have logged in successfully.");
     }
-
     public void logout() {
         System.out.println(name + " you havelogged out successfully.");
     }
 } 
     
+
 
