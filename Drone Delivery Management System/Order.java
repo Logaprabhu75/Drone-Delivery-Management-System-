45febc8470d4027a3dba723331692e1d4facb8a1
Logaprabhu75 <logaprabhu549@gmail.com>
2025-09-21 01:27:31 +0530

    Update Order.java

--- Drone Delivery Management System/Order.java
+++ Drone Delivery Management System/Order.java
@@ -1,16 +1,15 @@
 public class Order {
-    private static int counter = 1000; // starting order ID
+    private static int counter = 0;
     private String orderId;
     private String deliveryAddress;
     private String status = "Pending";
-
     public Order(String deliveryAddress) {
-        this.orderId = "ORD" + counter; // e.g., ORD1000
+        this.orderId = "OrdId" + counter; 
         counter++;
         this.deliveryAddress = deliveryAddress;
     }
-
     public String getOrderId() { return orderId; }
     public String getStatus() { return status; }
     public void updateStatus(String newStatus) { status = newStatus; }
 }
+
