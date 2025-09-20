b13415130fe63d435c6936e82293b2aab28efcfb
Logaprabhu75 <logaprabhu549@gmail.com>
2025-09-21 02:28:55 +0530

    Update Admin.java

--- Drone Delivery Management System/Admin.java
+++ Drone Delivery Management System/Admin.java
@@ -2,14 +2,15 @@ public class Admin extends User {
     public Admin(int userId, String name) {
         super(userId, name);
     }
-    public Drone registerDrone(String droneId) {
+    public Drone registerdrone(String droneId) {
         System.out.println("Drone registered: " + droneId);
         return new Drone(droneId);
     }
-    public void assignDrone(Drone drone, Order order) {
+    public void assigndrone(Drone drone, Order order) {
         drone.assignOrder(order);
         order.updateStatus("Assigned Successfully ");
     }
 }
+
 
 
