bc46fb2a749ed2f80416da53145ed90207355389
Logaprabhu75 <logaprabhu549@gmail.com>
2025-09-21 17:00:49 +0530

    Update Drone.java

--- Drone Delivery Management System/Drone.java
+++ Drone Delivery Management System/Drone.java
@@ -1,3 +1,4 @@
+// class for the drone 
 public class Drone {
     private String droneId;
     private String status = "Available";
@@ -6,13 +7,15 @@ public class Drone {
     }
     public void assignorder(Order order) {
         status = "Not Available";
-        System.out.println("Drone " + droneId + " assigned to order " + order.getOrderId());
+        System.out.println("The drone id  " + droneId + "  is assigned to order id  " + order.getOrderId());
     }
+    // order status update
     public void updatestatus(String newStatus) {
         status = newStatus;
         System.out.println("Drone " + droneId + " status updated to " + status);
     }
 }
+
 
 
 
