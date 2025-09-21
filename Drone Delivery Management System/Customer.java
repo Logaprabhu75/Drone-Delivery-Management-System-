bfef9fd9b991f1dbf6b8dc68cf1f172d3ee66758
Logaprabhu75 <logaprabhu549@gmail.com>
2025-09-21 17:06:33 +0530

    Update Customer.java

--- Drone Delivery Management System/Customer.java
+++ Drone Delivery Management System/Customer.java
@@ -1,19 +1,23 @@
+// class for customer
 import java.util.*;
 public class Customer extends User {
     private List<Order> orderHistory = new ArrayList<>();
     public Customer(int userId, String name) {
         super(userId, name);
     }
+    //place order
     public Order placeorder(String address) {
         Order order = new Order(address);
         orderHistory.add(order);
-        System.out.println("Order placed and order id is " + order.getOrderId());
+        System.out.println("Order placed successfully and order id is " + order.getOrderId());
         return order;
     }
+    //tracking of order
     public void trackorder(Order order) {
         System.out.println("the status of the order id  " + order.getOrderId() + "is" + order.getStatus());
     }
 }
+
 
 
 
